fbd14bc51fb4b99e8747b79a445366694a7987e5
Nikhil Raj S <nikhil.raj@experionglobal.com>
2026-03-17 22:11:08 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-amber-400 pt-8 text-2xl font-medium text-red-500'>Content</div>
+    </>
+  )
+}
+
+export default App
